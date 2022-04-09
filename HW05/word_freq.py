eab31c19e81fbfa98505f66fbf5e7fb475846819
Qad00 <majidovkadridin@gmail.com>
2022-04-09 17:47:22 +0900

    maded dictionary with frequency of words

--- HW05/word_freq.py
+++ HW05/word_freq.py
@@ -4,16 +4,23 @@ import re
 
 if __name__=='__main__':
 	if(len(sys.argv) == 3): 
-		list_words = []
+		list_of_words = []
 		with open(sys.argv[1], 'r') as file: 
 			for line in file:
 				list_line_words = re.split(r'\s*[-=+/.,!#$@%^&\s\t\n]\s*', line.strip())
-				list_words.extend(list_line_words)
-				list_words = [elem for elem in list_words if elem != ''] 
+				list_of_words.extend(list_line_words)
+				list_of_words = [elem for elem in list_of_words if elem != ''] 
 	else:
 		print("usage: %s <text_filename> <number> " % sys.argv[0])  
 		exit()
 
 
-print(list_words)
- 
+print(list_of_words)
+print() 
+
+dictionary = {}
+for word in list_of_words:
+	dictionary[word] = dictionary.get(word, 0) + 1
+
+
+print(dictionary) 
